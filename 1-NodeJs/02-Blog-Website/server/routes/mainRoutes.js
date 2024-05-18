1f9b9acebbe84f8e1890fe3335484ad9a66d8888
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-18 19:53:23 +0100

    chore: Add routes for homepage and about page

--- 1-NodeJs/02-Blog-Website/server/routes/mainRoutes.js
+++ 1-NodeJs/02-Blog-Website/server/routes/mainRoutes.js
@@ -4,7 +4,17 @@ const router = express.Router();
 
 // Routes
 router.get("/", (req, res) => {
+    const locals = {
+        title: "Home",
+        description: "Welcome to the homepage",
+    };
     res.render("index");
 });
+
+
+router.get("/about", (req, res) => {
+    res.render("about");
+});
+
 
 export default router;
