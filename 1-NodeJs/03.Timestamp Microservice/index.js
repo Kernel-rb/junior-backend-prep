d4c89429f1b203610b12a5a8e9e3e086101ae538
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-19 10:45:27 +0100

    @Nodejs/timestamp-microservice done

--- 1-NodeJs/03.Timestamp Microservice/index.js
+++ 1-NodeJs/03.Timestamp Microservice/index.js
@@ -8,25 +8,25 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/",  (req, res)  => {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello",  (req, res) => {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", (req, res) => {
+  res.json({ greeting: 'hello API' });
 });
 
 const invalideDate = (formattedDate) => formattedDate.toUTCString() === "Invalid Date";
 
-app.get("/api/:date",  (req, res) => {
+app.get("/api/:date", (req, res) => {
   const date = req.params.date;
   let formattedDate = new Date(date);
   if (invalideDate(formattedDate)) {
@@ -41,14 +41,14 @@ app.get("/api/:date",  (req, res) => {
     utc: formattedDate.toUTCString()
   });
 });
-app.get("/api/",  (req, res) => {
+app.get("/api/", (req, res) => {
   const date = new Date();
   res.json({
     unix: date.getTime(),
     utc: date.toUTCString()
   });
 });
-app.get("/api",  (req, res) => {
+app.get("/api", (req, res) => {
   const date = new Date();
   res.json({
     unix: date.getTime(),
@@ -57,6 +57,6 @@ app.get("/api",  (req, res) => {
 });
 
 // Listen on port set in environment variable or default to 3000
-var listener = app.listen(process.env.PORT || 3000,  () =>  {
+var listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
