69f251bb57b37d0db23936d8a6f70a38c1a3cd1e
Kernel-rb <matabsaifeddine@gmail.com>
2024-05-23 14:29:52 +0200

    +

---
+++ 0-Notes/01.Full-HTTP-networking/async.js
@@ -0,0 +1,18 @@
+const craftingCompleteWait = 8000   
+const combiningMaterialsWait = 4000
+const smeltingIronBarsWait = 2000
+const shapingIronWait = 6000
+
+// Don't touch below this line
+
+setTimeout(() => console.log('Iron Longsword Complete!'), craftingCompleteWait)
+setTimeout(() => console.log('Combining Materials...'), combiningMaterialsWait)
+setTimeout(() => console.log('Smelting Iron Bars...'), smeltingIronBarsWait)
+setTimeout(() => console.log('Shaping Iron...'), shapingIronWait)
+
+console.log('Firing up the forge...')
+
+sleep(2500)
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
